d77f8b7891ad4b187e0749c4a8c2b582d07c35e4
Mori Keli <kelimbth@gmail.com>
2022-12-07 11:10:20 +0300

    created a login view

--- users/views.py
+++ users/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
-import pyrebase
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import UserLoginForm
+
+import pyrebase as firebase
 
 firebaseConfig = {
     "apiKey": "AIzaSyAGy2iRgkPP_PVOFYiAfESLgYb_eLrT6eU",
@@ -8,12 +11,16 @@ firebaseConfig = {
     "storageBucket": "django-3aca5.appspot.com",
     "messagingSenderId": "212972838840",
     "appId": "1:212972838840:web:ff68185a3545e5628e394d",
-    "measurementId": "G-WGEYCC1HFJ"
+    "measurementId": "G-WGEYCC1HFJ",
+    "databaseURL": "",
 }
 
-app = pyrebase.initialize_app(firebaseConfig)
+app = firebase.initialize_app(firebaseConfig)
 authenticate_user = app.auth()
 
+class UserLogin(LoginView):
+    authentication_form = UserLoginForm
+    template_name = 'users/login.html'
 
 def signup_view(request):
 
