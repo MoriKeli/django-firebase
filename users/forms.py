ace4b492beb723c877e978277a287bfa260e50c6
Mori Keli <kelimbth@gmail.com>
2022-12-07 11:57:55 +0300

    created new login and signup forms

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,51 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
 from django import forms
+from .models import CustomUser
 
 
-class UserLoginForm(AuthenticationForm):
-    email = forms.CharField(widget=forms.TextInput(attrs={'type': 'email'}), required=True)
-    password = forms.CharField(widget=forms.TextInput(attrs={'type': 'password'}), required=True)
-    
+class RegistrationForm(UserCreationForm):
+    """
+      Form for Registering new users 
+    """
+    email = forms.EmailField(max_length=60, help_text = 'Required. Add a valid email address')
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'username', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        """
+          specifying styles to fields 
+        """
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        # for field in (self.fields['email'],self.fields['username'],self.fields['password1'],self.fields['password2']):
+            # field.widget.attrs.update({'class': 'form-control '})
+
+
+class UserLoginForm(forms.ModelForm):
+    """
+      Form for Logging in  users
+    """
+    password  = forms.CharField(label= 'Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model  =  CustomUser
+        fields =  ('email', 'password')
+        widgets = {
+                   'email':forms.TextInput(attrs={'class':'form-control'}),
+                   'password':forms.TextInput(attrs={'class':'form-control'}),
+        }
+    def __init__(self, request=None, *args, **kwargs):
+        """
+          specifying styles to fields 
+        """
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        for field in (self.fields['email'],self.fields['password']):
+            field.widget.attrs.update({'class': 'form-control '})
+
+    def clean(self):
+        if self.is_valid():
+            password = self.cleaned_data.get('password')
+            if not authenticate(email=email, password=password):
+                raise forms.ValidationError('Invalid credentials!!!')
+
