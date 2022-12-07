5589d0a622c2763ac97f7de9fa5c415b74ba43e2
Mori Keli <kelimbth@gmail.com>
2022-12-07 10:41:47 +0300

    added firebaseConfig dictionary

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import pyrebase
 
-# Create your views here.
+firebaseConfig = {
+    "apiKey": "AIzaSyAGy2iRgkPP_PVOFYiAfESLgYb_eLrT6eU",
+    "authDomain": "django-3aca5.firebaseapp.com",
+    "projectId": "django-3aca5",
+    "storageBucket": "django-3aca5.appspot.com",
+    "messagingSenderId": "212972838840",
+    "appId": "1:212972838840:web:ff68185a3545e5628e394d",
+    "measurementId": "G-WGEYCC1HFJ"
+}
+
+app = pyrebase.initialize_app(firebaseConfig)
+authenticate_user = app.auth()
+
+
+def signup_view(request):
+
+    return render(request, 'users/signup.html')
+
+def homepage_view(request):
+
+    return render(request, 'user/homepage.html')
