d53bf0bd70f32b50f550b594c1a852a0a5339811
Mori Keli <kelimbth@gmail.com>
2022-12-07 12:15:13 +0300

    made a some changes

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.UserLogin.as_view(), name='login'),
+    # path('', views.UserLogin.as_view(), name='login'),
+    path('', views.login_view, name='login'),
     path('homepage/', views.homepage_view, name='homepage'),
     path('create-account/', views.signup_view, name='signup'),
 
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.views import LoginView, LogoutView
 from .forms import UserLoginForm
 
@@ -18,9 +19,32 @@ firebaseConfig = {
 app = firebase.initialize_app(firebaseConfig)
 authenticate_user = app.auth()
 
-class UserLogin(LoginView):
-    authentication_form = UserLoginForm
-    template_name = 'users/login.html'
+# class UserLogin(LoginView):
+#     authentication_form = UserLoginForm
+#     template_name = 'users/login.html'
+
+def  login_view(request):
+    form = UserLoginForm()
+    if request.user.is_authenticated:
+        return redirect('homepage')
+
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        email = request.POST.get('email')
+        password = request.POST.get('email')
+
+        try:
+            user = authenticate_user.sign_in_with_email_and_password(email=email, password=password)
+
+        except:
+            messages.error(request, 'INVALID CREDENTIALS! Username and password may be case-sensitive.')
+            return redirect('login')
+
+
+    context = {'form': form}
+    return render(request, "users/login.html", context)
+
+
 
 def signup_view(request):
 
